fe6b4fa4d345c70be2800f74135dc08b06b5ec9e
Husnain Majeed <husnainm3@gmail.com>
2022-12-20 18:50:26 +0000

    Added a Tester class in the Network project to make sure code is working

---
+++ Network Checkers/Tester.cs
@@ -0,0 +1,18 @@
+﻿namespace Network
+{
+    public class Tester
+    {
+        static void Main()
+        {
+            var address = "192.168.1.253";
+            var timeout = 1000;
+            var ping = new NetPinger(address, timeout);
+            var successStatus = ping.IsSuccessful;
+
+            if (successStatus)
+               Console.WriteLine("Ping was successful with time : " + ping.Time);
+            else
+                 Console.WriteLine("Ping was unsuccessful with time : " + ping.Time);
+        }
+    }
+}
