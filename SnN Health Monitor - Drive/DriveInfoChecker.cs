6b72f8117bc99c96ca5d68f9af1128b458b8aa9c
Husnain Majeed <husnainm3@gmail.com>
2022-12-20 19:47:05 +0000

    Starting working on DriveInfoChecker Class and GetFreeSpace Method

---
+++ SnN Health Monitor - Drive/DriveInfoChecker.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace Drive
+{
+    public class DriveInfoChecker
+    {
+
+        public int GetFreeSpace(string Driveletter)
+        {
+            var drive = new DriveInfo(Driveletter);
+            long rawFreeSpace = 0;
+            int freeSpace;
+
+            if (drive.IsReady)
+                rawFreeSpace = drive.AvailableFreeSpace;
+
+            freeSpace = (int)rawFreeSpace / MeasuringUnits.MB;
+            return freeSpace;
+        }
+
+
+    }
+}
---
+++ SnN Health Monitor - Drive/MeasureUnits.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Drive
+{
+    public static class MeasuringUnits
+    {
+        public const int KB = 1024;
+        public const int MB = 1048576;
+        public const int GB = 1073741824;
+        public const long TB = 1099511627776;
+    }
+}
