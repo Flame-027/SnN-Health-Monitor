58ba38ba55c1255f95e4a6ed1c9d36a45f9d37a2
Husnain Majeed <husnainm3@gmail.com>
2022-12-20 18:31:57 +0000

    Added Network Checkers Project and NetPinger Class

---
+++ Network Checkers/NetPinger.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Network_Checkers
+{
+    public class NetPinger
+    {
+        private readonly Ping _ping = new Ping();
+        public float Time { get; private set; }
+        public int StatusCode { get; private set; }
+        public bool IsSuccessful
+        {
+            get {return this.StatusCode == 0;}
+
+        }
+
+        public NetPinger(string address, int timeout)
+        {
+            if (string.IsNullOrEmpty(address) || timeout < 0) 
+                throw new InvalidOperationException();
+
+            this.SendPing(address, timeout);
+        }
+
+        private void SendPing(string address, int timeout)
+        {
+            PingReply reply = _ping.Send(address, timeout);
+
+            this.Time = reply.RoundtripTime;
+            this.StatusCode = (int)reply.Status;
+        }
+    }
+}
