3d47609f1d11f527fa2b8a8a719019948c250926
Husnain Majeed <husnainm3@gmail.com>
2022-12-23 18:33:16 +0000

    Create another version of GetFreeSpace with option to specify unit out

--- SnN Health Monitor - Drive/DriveInfoChecker.cs
+++ SnN Health Monitor - Drive/DriveInfoChecker.cs
@@ -15,5 +15,16 @@ namespace Drive
             long freeSpace = BytesToBinaryUnits.Convert(drive.AvailableFreeSpace, new string("MB"));
             return freeSpace;
         }
+
+        public long GetFreeSpace(string Driveletter, string unitOut)
+        {
+            var drive = new DriveInfo(Driveletter);
+
+            if (!drive.IsReady) 
+                throw new InvalidOperationException("The Drive is not ready or available");
+
+            long freeSpace = BytesToBinaryUnits.Convert(drive.AvailableFreeSpace, unitOut);
+            return freeSpace;
+        }
     }
 }
