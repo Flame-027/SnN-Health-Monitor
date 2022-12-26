c95525156f372f43bfb70caa112fb4cc7984c52a
Husnain Majeed <husnainm3@gmail.com>
2022-12-26 15:36:52 +0000

    Started creating properties and constructor of Drive class

--- SnN Health Monitor - Drive/Drive.cs
+++ SnN Health Monitor - Drive/Drive.cs
@@ -1,8 +1,12 @@
-﻿
-namespace Drive
+﻿namespace Drive
 {
     public class Drive
     {
+        public string DriveLetter { get; private set; }
 
+        public Drive(string driveLetter)
+        {
+            this.DriveLetter = driveLetter;
+        }
     }
 }
