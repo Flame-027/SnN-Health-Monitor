72df201b3c1479772b98cf43f6b9a94dd8a417c7
Husnain Majeed <husnainm3@gmail.com>
2022-12-26 15:50:42 +0000

    Created GetAvailableSpace method

--- SnN Health Monitor - Drive/DriveInfoChecker.cs
+++ SnN Health Monitor - Drive/DriveInfoChecker.cs
@@ -29,5 +29,31 @@ namespace Drive
             long freeSpace = BytesToBinaryUnits.Convert(drive.TotalFreeSpace, unitOut);
             return freeSpace;
         }
+
+        public static long GetAvailableSpace(string driveLetter)
+        {
+            // Add validation for drive letter later
+
+            var drive = new DriveInfo(driveLetter);
+            
+            if (!drive.IsReady) 
+                throw new InvalidOperationException("The Drive is not ready or available");
+
+            long availableSpace = BytesToBinaryUnits.Convert(drive.AvailableFreeSpace, new string("MB"));
+            return availableSpace;
+        }
+
+        public static long GetAvailableSpace(string driveLetter, string unitOut)
+        {
+            // Add validation for drive letter later
+
+            var drive = new DriveInfo(driveLetter);
+
+            if (!drive.IsReady) 
+                throw new InvalidOperationException("The Drive is not ready or available");
+
+            long availableSpace = BytesToBinaryUnits.Convert(drive.AvailableFreeSpace, unitOut);
+            return availableSpace;
+        }
     }
 }
