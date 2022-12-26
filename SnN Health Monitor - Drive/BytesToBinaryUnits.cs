32573a74aa9fd1b2912892ad7bf9d647cbaa0533
Husnain Majeed <husnainm3@gmail.com>
2022-12-26 17:58:14 +0000

    Reworked BytesToBinaryUnits Class and methods
    This was done to make sure that parameters are always valid when the classes methods are called and to improve efficiency.

--- SnN Health Monitor - Drive/BytesToBinaryUnits.cs
+++ SnN Health Monitor - Drive/BytesToBinaryUnits.cs
@@ -3,28 +3,27 @@ namespace Drive
 {
     public static class BytesToBinaryUnits
     {
-        private static readonly Dictionary<string, long> _unitsHashTable = new()
+        private static readonly long[] _unitsArray = 
         {
-            { "KB", 1024 },
-            { "MB", 1048576 },
-            { "GB", 1073741824 },
-            { "TB", 1099511627776 }
+            1024,
+            1048576,                            
+            1073741824,
+            1099511627776
         };
 
-        public static long Convert(long inputBytes, string unitOut)
-        {
-            if (!_unitsHashTable.ContainsKey(unitOut))
-                throw new InvalidOperationException("The unit parameter is incorrect");
+        // this list gets indexed by an enum as some values are too large for it to hold.
 
-            return inputBytes / _unitsHashTable[unitOut];
+        public static long Convert(long inputBytes, BinaryUnitsCode unitOut)
+        {
+            if (inputBytes <= 0)
+                throw new InvalidOperationException("The inputedBytes paramter is not valid");
+
+            return inputBytes / _unitsArray[(byte)unitOut];
         }
 
-        public static float GetUnitInBytes(string unit)
+        public static float GetUnitInBytes(BinaryUnitsCode unit)
         {
-            if (!_unitsHashTable.ContainsKey(unit))
-               throw new InvalidOperationException("The unit parameter is incorrect");
-
-            return _unitsHashTable[unit];
+            return _unitsArray[(byte)unit];
         }
     }
 }
---
+++ SnN Health Monitor - Drive/Enum-BinaryUnitsCode.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Drive
+{
+    public enum BinaryUnitsCode
+    {
+        KB = 0,
+        MB = 1,
+        GB = 2,
+        TB = 3
+    }
+
+    // The enum holds index's for a list instead of just holding the values directly as the TB value is too large for an enum
+}
