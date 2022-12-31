ca0d1f565838d1a316b9995c8f9f872beabb9af0
Husnain Majeed <husnainm3@gmail.com>
2022-12-31 17:28:02 +0000

    Added field _driveinfo so that it doesn't have be reassigned in methods

--- SnN Health Monitor - Drive/Drive.cs
+++ SnN Health Monitor - Drive/Drive.cs
@@ -4,9 +4,12 @@
     {
         public string DriveLetter { get; private set; }
 
+        private DriveInfo _driveinfo;
+
         public Drive(string driveLetter)
         {
             this.DriveLetter = driveLetter;
+            this._driveinfo = new(driveLetter);
         }
     }
 }
