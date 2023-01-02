0b93d068ef8f66ad6022648e116a512740343431
Husnain Majeed <husnainm3@gmail.com>
2023-01-02 16:44:00 +0000

    Updated Test Class

---
+++ SnN Health Monitor - Drive/Tester.cs
@@ -0,0 +1,13 @@
+﻿namespace Drive
+{
+    class Tester
+    {
+        static void Main()
+        {
+          var driveChecker = new DriveInfoChecker(new DriveInfo("C"));
+          Console.WriteLine(driveChecker.GetAvailableSpace(BinaryUnitsCode.GB));
+          Console.WriteLine(driveChecker.GetFreeSpacePercentage());
+        }
+
+    }
+}
